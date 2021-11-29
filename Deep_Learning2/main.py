d56b20579845b95f45237a8e50a49f9cdd524c7e
Dobre Bogdan <dobrebogdan98@gmail.com>
2021-11-29 17:36:45 +0200

    Added some code

--- Deep_Learning2/main.py
+++ Deep_Learning2/main.py
@@ -8,6 +8,9 @@ def evaluate():
 def full_train():
     pass
 
+
+import csv
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import os
@@ -17,16 +20,17 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-import csv
 
 
-batch_size = 32
-img_height = 128
-img_width = 55
-train_ds = tf.keras.utils.image_dataset_from_directory(
-  directory='train',
-  validation_split=0.2,
-  subset="training",
-  seed=123,
-  #image_size=(img_height, img_width),
-  batch_size=batch_size)
+files_and_labels = []
+with open('train.csv') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        image = cv2.imread(f"train/{row[0]}") / 255
+        label = row[1]
+        files_and_labels.append((image, label))
+
+data_size = len(files_and_labels)
+training_data_zie = int(data_size * 0.8)
+training_data = files_and_labels[:training_data_zie]
+validation_data = files_and_labels[training_data_zie:]
