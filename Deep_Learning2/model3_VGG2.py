f0b92565722246fc0bca5871aa9283473abb3bb4
Dobre Bogdan <dobrebogdan98@gmail.com>
2021-12-19 01:45:11 +0200

    New model

---
+++ Deep_Learning2/model3_VGG2.py
@@ -0,0 +1,77 @@
+# Submission accuracy: 0.52
+
+import csv
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+batch_size = 32
+img_height = 128
+img_width = 55
+data_dir = './train'
+num_classes = 5
+
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    data_dir,
+    seed=123,
+    image_size=(img_height, img_width),
+    batch_size=batch_size)
+
+class_names = train_ds.class_names
+
+AUTOTUNE = tf.data.AUTOTUNE
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+
+
+from tensorflow.keras.applications.vgg16 import VGG16
+
+base_model = VGG16(input_shape = (128, 55, 3), # Shape of our images
+include_top = False, # Leave out the last fully connected layer
+weights = 'imagenet')
+
+
+for layer in base_model.layers:
+    layer.trainable = False
+
+# Flatten the output layer to 1 dimension
+x = layers.Flatten()(base_model.output)
+
+# Add a fully connected layer with 512 hidden units and ReLU activation
+x = layers.Dense(512, activation='relu')(x)
+
+# Add a dropout rate of 0.5
+x = layers.Dropout(0.5)(x)
+
+# Add a final sigmoid layer with 1 node for classification output
+x = layers.Dense(num_classes , activation='sigmoid')(x)
+
+model = tf.keras.models.Model(base_model.input, x)
+
+model.compile(optimizer = 'adam',
+              loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True), metrics = ['acc'])
+
+vgghist = model.fit(train_ds, validation_data = train_ds, steps_per_epoch = 100, epochs = 10)
+
+output_data = []
+with open('test.csv') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        image_path = f"test/{row[0]}"
+        img = tf.keras.utils.load_img(
+            image_path, target_size=(img_height, img_width)
+        )
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)  # Create a batch
+
+        predictions = model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
+        label = np.argmax(score)
+        output_data.append([row[0], int(class_names[label])])
+
+with open('output.csv', 'w') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerow(['id', 'label'])
+    for output_row in output_data:
+        writer.writerow(output_row)
